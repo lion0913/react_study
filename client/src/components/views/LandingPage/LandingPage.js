5902b83f840f9dbc3ed5f7b037dc63b857c31acd
박다정 <yd1065@naver.com>
2021-06-12 14:32:21 +0900

    회원가입, 로그인 페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import axios from "axios";
 
 function LandingPage() {
+    // 랜딩페이지에 들어오자마자 실행함 -> get request를 서버에 보냄
+    useEffect(() => {
+        axios.get("/api/hello")
+            .then(response => console.log(response))
+    }, [])
+
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display : 'flex', justifyContent : 'center', alignItems : 'center', width : '100%', height : '100vh'
+        }}>
+            <h2>LandingPage 렌딩페지이잉</h2>
         </div>
     )
 }
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from "react-redux";
+import {loginUser} from '../../../_actions/user_action'
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        //서버에 보내야 하는 값들을 Email,Password라는 state가 갖고있다는 것을 알 수 있음
+        console.log("email : ",Email)
+        console.log("password : ",Password)
+
+        let body = {
+            email : Email,
+            password : Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    props.history.push("/")
+                } else {
+                    alert('Error')
+                }
+            })
+
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                  onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
